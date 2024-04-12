217cf41858ab2d42c3ee1dee5530e5e95359cc61
M4eStroD <146201707+M4eStroD@users.noreply.github.com>
2024-04-12 16:51:07 +0500

    corrects

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -9,16 +9,34 @@ public class Explosion : MonoBehaviour
 
     [SerializeField] private SpawnCubes _spawnCubes;
 
+    [SerializeField] private int _chanceExplode = 100;
+
     private void OnMouseUpAsButton()
     {
-        Explode();
-        Instantiate(_effect, transform.position, transform.rotation);
-        _spawnCubes.SpawnCube();
+        TryExplode();
         Destroy(gameObject);
+    }
+
+    private void TryExplode()
+    {
+        int hundredPercent = 100;
+        int degreeReduction = 2;
+
+        int randomNumber = Random.Range(0, hundredPercent + 1);
+
+        if (randomNumber <= _chanceExplode)
+        {
+            _chanceExplode /= degreeReduction;
+
+            Explode();
+            _spawnCubes.SpawnCube();
+        }
     }
 
     private void Explode()
     {
+        Instantiate(_effect, transform.position, transform.rotation);
+
         foreach (Rigidbody explodableObject in GetExplodableObjects())
             explodableObject.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
     }
